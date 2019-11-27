4151d5b916264d0d24210c6e16b3db6fd24c7e31
v.medvedev <v.medvedev@conundrum.ai>
2019-11-27 21:42:46 +0300

    ex3: move indent to functions args

--- src/main/java/ru/made/ex/three/SimpleSerializer.java
+++ src/main/java/ru/made/ex/three/SimpleSerializer.java
@@ -6,14 +6,12 @@ import java.util.Collection;
 
 public class SimpleSerializer implements Serializer {
     private final SerializationStrategy strategy;
-    private int indent;
 
     SimpleSerializer(SerializationStrategy strategy) {
         this.strategy = strategy;
-        indent = 0;
     }
 
-    private void appendWithIndent(StringBuilder builder, String s) {
+    private void appendWithIndent(StringBuilder builder, Integer indent, String s) {
         builder.append(" ".repeat(indent));
         builder.append(s);
     }
@@ -25,12 +23,13 @@ public class SimpleSerializer implements Serializer {
         return s;
     }
 
-    private void increaseIndent() {
-        indent += 2;
+    private Integer increaseIndent(Integer indent) {
+        return indent + 2;
     }
 
-    private void decreaseIndent() {
-        indent -= 2;
+    private Integer decreaseIndent(Integer indent) {
+
+        return indent - 2;
     }
 
     private boolean isObject(Object o) {
@@ -38,72 +37,72 @@ public class SimpleSerializer implements Serializer {
         return !(clazz.isPrimitive() || o instanceof String || clazz.isArray() || Collection.class.isAssignableFrom(clazz));
     }
 
-    private void serializeCollectionElement(StringBuilder builder, Object o, Integer i, Integer arrayLength) throws IllegalAccessException {
-        appendWithIndent(builder, strategy.getCollectionElementPrefix(i + 1));
-        doSerialize(builder, Array.get(o, i));
+    private void serializeCollectionElement(StringBuilder builder, Integer indent, Object o, Integer i, Integer arrayLength) throws IllegalAccessException {
+        appendWithIndent(builder, indent, strategy.getCollectionElementPrefix(i + 1));
+        doSerialize(builder, indent, Array.get(o, i));
         builder.append(trimSuffix(strategy.getCollectionElementSuffix(i + 1), i, arrayLength));
     }
 
-    private void serializeCollection(StringBuilder builder, Object o) throws IllegalAccessException {
+    private void serializeCollection(StringBuilder builder, Integer indent, Object o) throws IllegalAccessException {
         builder.append(strategy.getCollectionPrefix());
-        increaseIndent();
+        indent = increaseIndent(indent);
         int arrayLength = Array.getLength(o);
         for (int i = 0; i < arrayLength; i++) {
-            serializeCollectionElement(builder, o, i, arrayLength);
+            serializeCollectionElement(builder, indent, o, i, arrayLength);
         }
-        decreaseIndent();
-        appendWithIndent(builder, strategy.getCollectionSuffix());
+        indent = decreaseIndent(indent);
+        appendWithIndent(builder, indent, strategy.getCollectionSuffix());
     }
 
-    private void serializeFieldAsObject(StringBuilder builder, Object o, Field field, Integer i, Integer arrayLength) throws IllegalAccessException {
-        appendWithIndent(builder, strategy.getObjectPrefix(field.getName()));
-        doSerialize(builder, field.get(o));
-        appendWithIndent(builder, trimSuffix(strategy.getObjectSuffix(field.getName()), i, arrayLength));
+    private void serializeFieldAsObject(StringBuilder builder, Integer indent, Object o, Field field, Integer i, Integer arrayLength) throws IllegalAccessException {
+        appendWithIndent(builder, indent, strategy.getObjectPrefix(field.getName()));
+        doSerialize(builder, indent, field.get(o));
+        appendWithIndent(builder, indent, trimSuffix(strategy.getObjectSuffix(field.getName()), i, arrayLength));
     }
 
-    private void serializeField(StringBuilder builder, Object o, Field field, Integer i, Integer arrayLength) throws IllegalAccessException {
-        appendWithIndent(builder, strategy.getFiledPrefix(field.getName()));
-        doSerialize(builder, field.get(o));
+    private void serializeField(StringBuilder builder, Integer indent, Object o, Field field, Integer i, Integer arrayLength) throws IllegalAccessException {
+        appendWithIndent(builder, indent, strategy.getFiledPrefix(field.getName()));
+        doSerialize(builder, indent, field.get(o));
         builder.append(trimSuffix(strategy.getFieldSuffix(field.getName()), i, arrayLength));
     }
 
 
-    private void serializeObject(StringBuilder builder, Object o, Class<?> clazz) throws IllegalAccessException {
+    private void serializeObject(StringBuilder builder, Integer indent, Object o, Class<?> clazz) throws IllegalAccessException {
         Field[] fields = clazz.getDeclaredFields();
         int arrayLength = fields.length;
         for (int i = 0; i < arrayLength; i++) {
             fields[i].setAccessible(true);
             if (isObject(fields[i].get(o))) {
-                serializeFieldAsObject(builder, o, fields[i], i, arrayLength);
+                serializeFieldAsObject(builder, indent, o, fields[i], i, arrayLength);
             } else {
-                serializeField(builder, o, fields[i], i, arrayLength);
+                serializeField(builder, indent, o, fields[i], i, arrayLength);
             }
         }
     }
 
-    private void doSerialize(StringBuilder builder, Object o) throws IllegalAccessException {
+    private void doSerialize(StringBuilder builder, Integer indent, Object o) throws IllegalAccessException {
         Class<?> clazz = o.getClass();
         if (clazz.isPrimitive() || o instanceof String) {
             builder.append(strategy.getScalarValue(o.toString()));
         } else if (clazz.isArray() || Collection.class.isAssignableFrom(clazz)) {
-            serializeCollection(builder, o);
+            serializeCollection(builder, indent, o);
         } else {
-            increaseIndent();
-            serializeObject(builder, o, clazz);
-            decreaseIndent();
+            indent = increaseIndent(indent);
+            serializeObject(builder, indent, o, clazz);
         }
     }
 
     @Override
     public String serialize(Object o) {
         StringBuilder builder = new StringBuilder();
-        appendWithIndent(builder, strategy.getFilePrefix(o.getClass().getName()));
+        Integer indent = 0;
+        builder.append(strategy.getFilePrefix(o.getClass().getName()));
         try {
-            doSerialize(builder, o);
+            doSerialize(builder, indent, o);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-        appendWithIndent(builder, strategy.getFileSuffix(o.getClass().getName()));
+        builder.append(strategy.getFileSuffix(o.getClass().getName()));
         return builder.toString();
     }
 }
